28929aaf056e9c370f78bfba2c55021ab005820f
Paul Flowers <pcflowers@live.com>
2023-01-10 15:46:49 -0600

    syntax changes

--- test/EloRegressionTest/eloRegressionTest.sh
+++ test/EloRegressionTest/eloRegressionTest.sh
@@ -6,6 +6,7 @@ echo "Starting ELO testing script"
 cp ../../Menace MenaceCurrent
 cp ../../Menace Menace
 
+<<comment
 if [ ! -f Menace ]; then
 
     echo "Menace not found. Cloning from github and building"
@@ -31,9 +32,9 @@ if [ ! -f Menace ]; then
 	cp Menace ../Menace
 	cd ..
 fi
+comment
 
-
-./cutechess-cli.sh -engine cmd=MenaceCurrent -engine cmd=../Menace -each proto=uci tc=100/1+0.01 -rounds 1000 -pgnout matchOuput.txt -recover -repeat -openings file=openings.pgn | while read line; do
+./cutechess-cli.sh -engine cmd=MenaceCurrent -engine cmd=Menace -each proto=uci tc=100/1+0.01 -rounds 750 -pgnout matchOuput.txt -recover -repeat -openings file=openings.pgn | while read line; do
 
 		
 	echo "$line"
