7510e06a4c308e1f3f660059ed4ad94a07677a8a
Paul Flowers <pcflowers@live.com>
2023-01-13 15:38:23 -0600

    Update regression testing

--- test/EloRegressionTest/eloRegressionTest.sh
+++ test/EloRegressionTest/eloRegressionTest.sh
@@ -34,7 +34,7 @@ if [ ! -f Menace ]; then
 fi
 comment
 
-./cutechess-cli.sh -engine cmd=MenaceCurrent -engine cmd=Menace -each proto=uci tc=100/1+0.01 -rounds 50 -pgnout matchOuput.txt -recover -repeat -openings file=openings.pgn | while read line; do
+./cutechess-cli.sh -engine cmd=MenaceCurrent -engine cmd=Menace -each proto=uci tc=100/1+0.01 -rounds 20 -pgnout matchOuput.txt -recover -repeat -debug -openings file=openings.pgn | while read line; do
 
 		
 	echo "$line"
